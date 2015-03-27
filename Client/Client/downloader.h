dade3f2a6f15126659c9dadeac859f7f33d76134
lamckalex <lam.ck.alex@gmail.com>
2015-03-26 20:34:48 -0700

    Added the actual files for download dialog.

---
+++ Client/Client/downloader.h
@@ -0,0 +1,30 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
+#include <QDialog>
+#include <QFile>
+
+namespace Ui {
+class Downloader;
+}
+
+class Downloader : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Downloader(QWidget *parent = 0);
+    ~Downloader();
+    int SetFileName(QString);
+    int SetBytesExpected(int);
+    void StartDownload();
+
+private:
+    Ui::Downloader *ui;
+    QString filename;
+    QFile * file;
+    int bytesExpected;
+    int bytesReceived;
+};
+
+#endif // DOWNLOADER_H
