bc483530a489f4405691636fbc495b00fb827fd1
Filip <fgutica@gmail.com>
2015-03-31 14:24:50 -0700

    no message

---
+++ Client/build-Client-Desktop_Qt_5_4_0_MSVC2012_OpenGL_32bit-Release/ui_downloader.h
@@ -0,0 +1,67 @@
+/********************************************************************************
+** Form generated from reading UI file 'downloader.ui'
+**
+** Created by: Qt User Interface Compiler version 5.3.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DOWNLOADER_H
+#define UI_DOWNLOADER_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Downloader
+{
+public:
+    QPushButton *CancelButton;
+    QProgressBar *ProgressBar;
+    QLabel *DownloadMessage;
+
+    void setupUi(QDialog *Downloader)
+    {
+        if (Downloader->objectName().isEmpty())
+            Downloader->setObjectName(QStringLiteral("Downloader"));
+        Downloader->resize(400, 153);
+        CancelButton = new QPushButton(Downloader);
+        CancelButton->setObjectName(QStringLiteral("CancelButton"));
+        CancelButton->setGeometry(QRect(300, 120, 75, 23));
+        ProgressBar = new QProgressBar(Downloader);
+        ProgressBar->setObjectName(QStringLiteral("ProgressBar"));
+        ProgressBar->setGeometry(QRect(30, 80, 341, 23));
+        ProgressBar->setValue(24);
+        DownloadMessage = new QLabel(Downloader);
+        DownloadMessage->setObjectName(QStringLiteral("DownloadMessage"));
+        DownloadMessage->setGeometry(QRect(30, 20, 341, 51));
+
+        retranslateUi(Downloader);
+
+        QMetaObject::connectSlotsByName(Downloader);
+    } // setupUi
+
+    void retranslateUi(QDialog *Downloader)
+    {
+        Downloader->setWindowTitle(QApplication::translate("Downloader", "Dialog", 0));
+        CancelButton->setText(QApplication::translate("Downloader", "Cancel", 0));
+        DownloadMessage->setText(QApplication::translate("Downloader", "Downloading file: ", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Downloader: public Ui_Downloader {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DOWNLOADER_H
