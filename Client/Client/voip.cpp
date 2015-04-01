d512d3a8983964b7c6e9713b62833b12566f7681
spelka <sebastian.pelka@hotmail.com>
2015-03-31 18:07:27 -0700

    VOIP maybe working... push before first testing
    Signed-off-by: spelka <sebastian.pelka@hotmail.com>

--- Client/Client/voip.cpp
+++ Client/Client/voip.cpp
@@ -1,7 +1,8 @@
 #include "voip.h"
 #include "ui_voip.h"
 #include "configure.h"
-#include <QDebug>
+#include "audioplayer.h"
+
 Voip::Voip(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Voip)
@@ -26,10 +27,51 @@ void Voip::on_makeCallBtn_clicked()
 
     }
     qDebug() << "IP" + ip + "\nPORT" + port;
-    //ConnectTCP(ip, port);
 
+    udpSocket = new QUdpSocket(this);
+    udpSocket->bind(QHostAddress(ip), port.toInt());
+
+    AudioPlayer* ap = new AudioPlayer(udpSocket);
+    ap->show();
 }
+
+void Voip::recordAudio(QUdpSocket *udpSocket)
+{
+    audioRecorder = new QAudioRecorder(this);
+    audioProbe = new QAudioProbe();
+
+    connect(audioProbe, SIGNAL(audioBufferProbed(QAudioBuffer)), this, SLOT(processBuffer(QAudioBuffer)));
+
+    QAudioEncoderSettings settings;
+
+    settings.setSampleRate(44100);
+    settings.setChannelCount(2);
+    settings.setBitRate(16);
+    settings.setCodec("audio/pcm");
+    //settings.setByteOrder(QAudioFormat::LittleEndian);
+    //settings.setSampleType(QAudioFormat::UnSignedInt);
+
+    audioRecorder->setEncodingSettings(settings, QVideoEncoderSettings(), NULL);
+    audioRecorder->record();
+
+    //read data into a QByteArray
+
+
+
+    //shove the contents of the QByteArray into the UDP socket
+
+    //profit
+}
+
 void Voip::on_acceptCallBtn_clicked()
 {
     qDebug() << "Call Received";
+}
+
+void Voip::processBuffer(QAudioBuffer buffer)
+{
+    QByteArray *audioByteArray;
+    *audioByteArray = audioByteArray->fromRawData((char*) buffer.data(), buffer.byteCount());
+
+    udpSocket->writeDatagram((const QByteArray&)audioByteArray, (const QHostAddress&)(ip), (quint16)(port.toInt()));
 }
--- Client/Client/voip.h
+++ Client/Client/voip.h
@@ -2,6 +2,12 @@
 #define VOIP_H
 
 #include <QDialog>
+#include <QDebug>
+#include <QUdpSocket>
+#include <QAudioRecorder>
+#include <QAudioProbe>
+#include <QAudioBuffer>
+#include <QByteArray>
 
 namespace Ui {
 class Voip;
@@ -14,16 +20,20 @@ class Voip : public QDialog
 public:
     explicit Voip(QWidget *parent = 0);
     ~Voip();
+    void recordAudio(QUdpSocket *udpSocket);
 
 private slots:
     void on_makeCallBtn_clicked();
-
     void on_acceptCallBtn_clicked();
+    void processBuffer(QAudioBuffer buffer);
 
 private:
     Ui::Voip *ui;
     QString ip;
     QString port;
+    QAudioRecorder* audioRecorder;
+    QAudioProbe* audioProbe;
+    QUdpSocket *udpSocket;
 };
 
 #endif // VOIP_H
